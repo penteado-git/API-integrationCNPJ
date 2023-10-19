95139a3dd3e4ea98147c816ff218c4aef1fe6aaf
Gabriel Penteado <105011647+penteado-git@users.noreply.github.com>
2023-10-19 14:03:10 -0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -3,10 +3,10 @@ import xmlrpc.client as client
 
 
 uid = 2
-url = "http://192.168.196.125:8070/"
-db = 'odoo2'
-username = 'Administrator'
-password = 'Solere1122!@'
+url = "http:// #URL do local host da aplicação "
+db = ''
+username = ''
+password = ''
 models = client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
 
@@ -16,6 +16,6 @@ def crm_execute(model, method, *args):
 
 class Variables:
     def __init__(self):
-        self.mongo_aliest = MongoClient('mongodb://ali2:Teste%402020@192.168.196.101:27017/?authMechanism=DEFAULT')
+        self.mongo_aliest = MongoClient('mongodb:/#caminho para o banco de dados mongoDB/')
         self.crm_aliest = crm_execute
-        self.cnpja_auth = {'Authorization': '3e7fa31b-2e5c-4ca5-b0c6-26a8f1745d68-fbd473bc-efea-4249-8d5f-06da38d1025d'}
+        self.cnpja_auth = {'Authorization': '#código de autorização'}
